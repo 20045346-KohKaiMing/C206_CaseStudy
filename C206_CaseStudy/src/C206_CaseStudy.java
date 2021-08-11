8d16bcd59e3e1600c0f1e3148644afc67bbc0436
akash <akash@DESKTOP-1J1E2BI>
2021-08-11 13:21:20 +0800

    Completed AddPackage method and test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
+import java.util.Date;
 
 public class C206_CaseStudy {
+	
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Package> PackageList = new ArrayList<Package>();
 
 	}
 	
-	
+	//================================ Add Package ==================================================
+	public static Package inputPackage() {
+		String Code = Helper.readString("Enter package code :");
+		String description = Helper.readString("Enter description :");
+		Date startDate = Helper.readDate("Enter Start Date :");
+		Date endDate = Helper.readDate("Enter End Date :");
+		int amount = Helper.readInt("Enter package amount :");
+		
+		Package X = new Package(Code, description,  startDate, endDate, amount);
+		return X;
+	}
+	public static void addPackage(ArrayList<Package> PackageList, Package X) {
+		
+		PackageList.add(X);
+		
+	}
 	
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,5 +22,37 @@ public class C206_CaseStudyTest {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
+	
+	
+	@Test
+	public void addPackageTest() {
+		
+		ArrayList<Package> PackageList = new ArrayList<Package>();
+		
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Check if there is valid Camcorder arraylist to add to", PackageList);
+		Date start = new Date();
+		Date end = new Date();
+		Package X = new Package("PK001","Test Package",  start , end, 3 );
+		Package Y = new Package("PK002","Test Package 2",  start , end, 7 );
+		
+		
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.addPackage(PackageList, X);
+		assertEquals("Check that Package arraylist size is 1", 1, PackageList.size());
+		assertSame("Check that Package is added", X, PackageList.get(0));
+				
+		
+		
+		//Add another item. test The size of the list is 2? -normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addPackage(PackageList, Y);
+		assertEquals("Check that Package arraylist size is 2", 2, PackageList.size());
+		assertSame("Check that Package is added", Y, PackageList.get(1));
+		
+		
+			}
+	}
 
-}
+
