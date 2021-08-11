9ba4006cf3639aea50742feb2ea67bc077b2d40e
20045346 <20045346@DESKTOP-02J14N2>
2021-08-11 13:19:25 +0800

    added arraylist(quotation request) and some Junit for it

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,10 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	ArrayList<RequestQuotation> rqList;
 
 	@Before
 	public void setUp() throws Exception {
@@ -19,5 +23,27 @@ public class C206_CaseStudyTest {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
+	
 
+	//Add Request for Quote
+	public void addRequestQuotation() {
+		// Item list is not null, so that can add a new item - boundary
+		assertNotNull("Check if there is valid Request for quotation arraylist to add to", rqList);
+		
+	}
+	
+	public void viewRequestQuotation() {
+		// Test if Item list is not null but empty -boundary
+		assertNotNull("Check if there is valid Request for Quotation arraylist to add to", rqList);
+		
+		
+	}
+	
+	public void deleteRequestQuotation() {
+		// Test if Item list is not null but empty -boundary
+		assertNotNull("Check if there is valid Request for Quotation arraylist to delete from", rqList);
+
+	}
+	
 }
+
